9e2e2cd9b05359f393d1f9c61ec4be9a5441d25d
Saul Diaz <diaz.saul.lomeli@gmail.com>
2022-11-12 16:57:02 -0800

    Updates

---
+++ precache-manifest.6e4baff26174817f2f94e40b849ba77f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "87cfda331dc7ff99443e",
+    "url": "/interstellar/static/css/main.9ea1c2f9.chunk.css"
+  },
+  {
+    "revision": "87cfda331dc7ff99443e",
+    "url": "/interstellar/static/js/main.87cfda33.chunk.js"
+  },
+  {
+    "revision": "575ef48bf44201abfebc",
+    "url": "/interstellar/static/js/1.575ef48b.chunk.js"
+  },
+  {
+    "revision": "4918abfdc56329242744",
+    "url": "/interstellar/static/js/runtime~main.4918abfd.js"
+  },
+  {
+    "revision": "ec767506836d4c6d513fed3648826ed2",
+    "url": "/interstellar/static/media/astroVideo.ec767506.mp4"
+  },
+  {
+    "revision": "d8a2634f1a4ff69a10c06f9c91348884",
+    "url": "/interstellar/static/media/astroImage.d8a2634f.png"
+  },
+  {
+    "revision": "02a311ecf93cc5992456a67f97b536fe",
+    "url": "/interstellar/static/media/heroImage.02a311ec.jpg"
+  },
+  {
+    "revision": "ae684cc9650a196859426dca8da5551d",
+    "url": "/interstellar/index.html"
+  }
+];
